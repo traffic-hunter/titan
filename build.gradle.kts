88b118155fbcfea9290e3dd7473c8d806c46e565
swager253 <swager253@naver.com>
2025-04-25 00:47:32 +0900

    [no-issue] Add dependencies (lombok, slf4j)

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,21 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+
+}
+
 subprojects {
     apply(plugin = "java")
 
     dependencies {
         testImplementation(platform("org.junit:junit-bom:5.10.0"))
         testImplementation("org.junit.jupiter:junit-jupiter")
+
+        compileOnly("org.projectlombok:lombok:1.18.38")
+        annotationProcessor("org.projectlombok:lombok:1.18.38")
+
+        implementation("org.slf4j:slf4j-api:2.0.17")
     }
 }
 
