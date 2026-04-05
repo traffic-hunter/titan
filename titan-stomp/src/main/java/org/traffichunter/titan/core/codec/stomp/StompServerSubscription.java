c82da89d3753666864b1cec297ad7a68f073435c
yun <swager253@naver.com>
2026-04-05 23:27:33 +0900

    extract shared subscription registry for client and server

---
+++ titan-stomp/src/main/java/org/traffichunter/titan/core/codec/stomp/StompServerSubscription.java
@@ -0,0 +1,62 @@
+/*
+The MIT License
+
+Copyright (c) 2025 traffic-hunter
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package org.traffichunter.titan.core.codec.stomp;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.traffichunter.titan.core.channel.Subscription;
+import org.traffichunter.titan.core.channel.stomp.StompClientConnection;
+import org.traffichunter.titan.core.util.Destination;
+
+/**
+ * @author yun
+ */
+@Getter
+public class StompServerSubscription extends Subscription implements StompSubscription {
+
+    private final String ackMode;
+    private final StompClientConnection connection;
+
+    @Builder
+    public StompServerSubscription(
+            Destination destination,
+            String id,
+            String ackMode,
+            StompClientConnection connection
+    ) {
+        super(destination, id);
+        this.ackMode = ackMode;
+        this.connection = connection;
+    }
+
+    @Override
+    public String id() {
+        return getId();
+    }
+
+    @Override
+    public Destination destination() {
+        return getDestination();
+    }
+}
