45d95c428d09ce9099231a106e65b423cbcb0d3f
yun <swager253@naver.com>
2026-08-04 22:41:17 +0900

    Rely on Titan client transitive API

--- titan-spring-client/build.gradle.kts
+++ titan-spring-client/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 
 dependencies {
     api(project(":titan-client"))
-    implementation(project(":core"))
 
     implementation(project.libs.spring.boot.autoconfigure)
     implementation(project.libs.spring.context)
