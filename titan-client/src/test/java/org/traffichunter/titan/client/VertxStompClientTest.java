7ffc2354332ecb69ceb9f0714909b27f092c0100
yun <swager253@naver.com>
2026-08-08 16:07:00 +0900

    Stabilize Vert.x reconnect test

--- titan-client/src/test/java/org/traffichunter/titan/client/VertxStompClientTest.java
+++ titan-client/src/test/java/org/traffichunter/titan/client/VertxStompClientTest.java
@@ -15,6 +15,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.awaitility.Awaitility.await;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.after;
 import static org.mockito.ArgumentMatchers.any;
@@ -153,6 +154,8 @@ class VertxStompClientTest {
         connectionDroppedHandler(firstConnection).handle(firstConnection);
 
         verify(nativeClient, timeout(1000).times(3)).connect(option.port(), option.host());
+        await().atMost(2, TimeUnit.SECONDS)
+                .untilAsserted(() -> assertThat(client.isConnected()).isTrue());
         assertThat(driver.channel()).isSameAs(restoredConnection);
         assertThat(client.connection()).isNotSameAs(initialConnection);
 
